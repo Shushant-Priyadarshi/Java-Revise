e7cb39ef837b9b53e63bd6c05eec9b4ccf3fc9df
Shushant-Priyadarshi <profshushant@gmail.com>
2024-12-29 20:32:23 +0530

    revisited inheritance

---
+++ src/oops/inheritance/Animal.java
@@ -0,0 +1,10 @@
+package oops.inheritance;
+
+public class Animal {
+    String name;
+    int legs;
+
+    void eat(){
+        System.out.println(this.name+" has "+this.legs+" legs");
+    }
+}
---
+++ src/oops/inheritance/Main.java
@@ -0,0 +1,11 @@
+package oops.inheritance;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Dog d1 = new Dog();
+        d1.name = "Tubby";
+        d1.legs =4;
+        d1.eat();
+    }
+}
